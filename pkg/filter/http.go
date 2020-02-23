9beeb95d5cd2af51d464ee09f2df1d5e8fc25ba5
jianggushi <rogerjiangx@outlook.com>
2020-02-23 12:27:28 +0800

    feat: add http header for http request

--- pkg/filter/http.go
+++ pkg/filter/http.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/jianggushi/proxypool/pkg/model"
+	"github.com/jianggushi/proxypool/pkg/request"
 )
 
 func RequestBaidu(proxy *model.Proxy) (int, error) {
@@ -32,6 +33,15 @@ func requestSite(proxy *model.Proxy, siteurl string) (int, error) {
 	if err != nil {
 		return 0, err
 	}
+	u, err := url.Parse(siteurl)
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("Host", u.Host)
+	req.Header.Set("Referer", fmt.Sprintf("%s://%s", u.Scheme, u.Host))
+	// req.Header.Set("Accept", "*/*")
+	// req.Header.Set("Accept-Encoding", "gzip")
+	req.Header.Set("User-Agent", request.RandomUA())
 	t1 := time.Now()
 	resp, err := client.Do(req)
 	if err != nil {
--- pkg/request/request.go
+++ pkg/request/request.go
@@ -1,7 +1,9 @@
 package request
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -11,11 +13,17 @@ var (
 	}
 )
 
-func Get(url string) (resp *http.Response, err error) {
-	req, err := http.NewRequest("GET", url, nil)
+func Get(rawurl string) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", rawurl, nil)
 	if err != nil {
 		return nil, err
 	}
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Host", u.Host)
+	req.Header.Set("Referer", fmt.Sprintf("%s://%s", u.Scheme, u.Host))
 	// req.Header.Set("Accept", "*/*")
 	// req.Header.Set("Accept-Encoding", "gzip")
 	req.Header.Set("User-Agent", RandomUA())
